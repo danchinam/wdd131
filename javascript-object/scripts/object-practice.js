4575b1614d874cba3a875dce388e5222bd841286
danchinam <dchinagorom@byupathway.edu>
2025-04-21 13:35:59 +0100

    AM-WEBSITE

---
+++ javascript-object/scripts/object-practice.js
@@ -0,0 +1,19 @@
+const person = {
+  name: {
+    first: "bob",
+    last: "smith",
+  }
+};
+  /*name: ["Bob", "Smith"],
+  age: 32,
+  bio: function () {
+    console.log(`${this.name[0]} ${this.name[1]} is ${this.age} years old.`);
+  },
+  introduceSelf: function () {
+    console.log(`Hi! I'm ${this.name[0]}.`);
+  },
+}; */
+
+
+
+  
